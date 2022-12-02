1d9946fe10f87293a69a2b60b7e10b5858c0af8d
Yatin <yatin.nandwani@gmail.com>
2022-12-02 05:20:09 +0000

    cleaning configs

--- conf/knapsack/ilploss/10_orig/gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-export seed
-for seed in {0..9}
-do
-	envsubst < template.yaml > ${seed}.yaml
-    echo "timeout 12.5h python trainer.py --config conf/neurips/knapsack/binary/ilploss/10_orig/${seed}.yaml > logs/neurips/knapsack/binary/ilploss/10_orig_${seed}.log 2>&1" >> orig_10_ilploss.sh
-done
